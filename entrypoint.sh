4b5c0bbab10dc08013584ba486552b6d6c05f568
Zhiyuan Zheng <me@xmflsct.com>
2019-10-18 14:38:21 +0200

    Update shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 yarn
 gatsby build
-yarn run gh-pages -d public -b $1 -u '$3 <$4>' -r https://$2@github.com/${{ GITHUB_REPOSITORY }}.git -m 'Build of ${{ GITHUB_SHA }}'
+yarn run gh-pages -d public -b $1 -u ''"$3"' ''<'"$4"'>' -r 'https://'"$2"'@github.com/${{ GITHUB_REPOSITORY }}.git' -m 'Build of ${{ GITHUB_SHA }}'
