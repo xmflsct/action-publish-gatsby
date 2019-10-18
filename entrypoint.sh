151d0a458bd38dcc724f981fa4792d5a49679d10
Zhiyuan Zheng <me@xmflsct.com>
2019-10-18 15:53:46 +0200

    Move env to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 yarn
 gatsby build
-yarn run gh-pages -d public -b $1 -u ''"$3"' ''<'"$4"'>' -r 'https://'"$2"'@github.com/'"$5"'.git' -m 'Build of '"$5"''
+yarn run gh-pages -d public -b $1 -u ''"$3"' ''<'"$4"'>' -r 'https://'"$2"'@github.com/'"$GITHUB_REPOSITORY"'.git' -m 'Build of '"$GITHUB_SHA"''
